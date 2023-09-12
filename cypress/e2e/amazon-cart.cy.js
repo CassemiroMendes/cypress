a24c914e6459eb36ce87aa70a0ca0f802206a0a1
Luiz Mendes <luiz.mendes@landisgyr.com>
2023-09-12 09:22:45 -0300

    Refacturing Test Cases

--- cypress/e2e/amazon-cart.cy.js
+++ cypress/e2e/amazon-cart.cy.js
@@ -2,11 +2,9 @@ import { faker } from '@faker-js/faker/locale/en';
 
 
 describe('Adding Itens to Shopping Cart', () => {
-  it('Adding two itens from home screen', () => {
-    let productname = 'ZIUMIER Gaming Headset with Microphone, Compatible with PS4 PS5 Xbox One PC Laptop, Over-Ear Headphones with LED RGB Light, Noise Canceling Mic, 7.1 Stereo Surround Sound';    
+  it('Adding one item from home screen', () => {
+    let productname = 'ZIUMIER Gaming Headset with Microphone, Compatible with PS4 PS5 Xbox One PC Laptop, Over-Ear Headphones with LED RGB Light, Noise Canceling Mic, 7.1 Stereo Surround Sound';
     let shortproductname = 'ZIUMIER Gaming Headset with Microphone, Compatible with PS…'
-    let shortproductname2 = 'Xbox Series S – 512GB SSD All-Digital Gaming Console – 1440p…'
-    let productname2 = 'Xbox Series S – 512GB SSD All-Digital Gaming Console – 1440p Gaming – 4K Streaming – Robot White'
     cy.visit('https://www.amazon.com/ref=nav_bb_logo');
 
     cy.get('#desktop-grid-1').contains('Headsets').click();
@@ -15,10 +13,19 @@ describe('Adding Itens to Shopping Cart', () => {
     cy.get('#NATC_SMART_WAGON_CONF_MSG_SUCCESS').contains('Added to Cart');
     cy.get('#sw-gtc').contains('Go to Cart').click();
     cy.get('*[class^="a-truncate-cut"]').contains(shortproductname);
-    cy.get('#nav-logo').click();
+  })
+})
+
+describe('Adding Item with the search box to Shopping Cart', () => {
+  it('Adding one item from search box', () => {
+
+    let shortproductname = 'Xbox Series S – 512GB SSD All-Digital Gaming Console – 1440p…'
+    let productname = 'Xbox Series S – 512GB SSD All-Digital Gaming Console – 1440p Gaming – 4K Streaming – Robot White'
+    cy.visit('https://www.amazon.com/ref=nav_bb_logo');
+
     cy.get('#twotabsearchtextbox').type('xbox');
     cy.get('#nav-search-submit-text').click();
-    cy.get('*[class^="sg-col-inner"]').contains(productname2).click();
+    cy.get('*[class^="sg-col-inner"]').contains(productname).click();
     cy.get('*[class^="a-button-inner"]').contains('Add to Cart').click();
     cy.get('#NATC_SMART_WAGON_CONF_MSG_SUCCESS').contains('Added to Cart');
     cy.get('#sw-gtc').contains('Go to Cart').click();
